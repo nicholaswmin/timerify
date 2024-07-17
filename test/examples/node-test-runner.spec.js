cf1af27179a551a1aadc46844612d10aa2b8a8b5
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-17 13:05:22 +0300

    doc: avoid testing for max

--- test/examples/node-test-runner.spec.js
+++ test/examples/node-test-runner.spec.js
@@ -4,24 +4,25 @@ import { timerify } from '../../index.js'
 const fibonacci = n => n < 1 ? 0 : n <= 2
   ? 1 : fibonacci(n - 1) + fibonacci(n - 2)
 
-await test('perf: #fibonacci(20) x 10 times', async t => {
-  const timed_fibonacci = timerify(fibonacci)
+const timed_fibonacci = timerify(fibonacci)
 
+test('perf: #fibonacci(20) x 10 times', async t => {
   t.beforeEach(() => {
+    console.log('called')
     for (let i = 0; i < 10; i++)
       timed_fibonacci(20)
   })
 
-  await t.test('completes each cycle quickly', t => {
+  await t.test('called 10 times', t => {
+    const callCount = timed_fibonacci.histogram_ms.count
+
+    t.assert.strictEqual(callCount, 10)
+  })
+
+  await t.test('runs quickly, on average', t => {
     const mean = timed_fibonacci.histogram_ms.mean
 
     t.assert.ok(mean < 30, `mean: ${mean} ms exceeded 30ms threshold`)
-  })
-
-  await t.test('never exceeds 100ms', t => {
-    const max = timed_fibonacci.histogram_ms.max
-
-    t.assert.ok(max < 100, `max: ${max} ms exceeded 100ms threshold`)
   })
 
   await t.test('has consistent running times', t => {
